6ececd5b0bc28c5ecc40bb3c4f7113956910e81a
AbdulsispnShakoor <shakoor.sispn@gmail.com>
2025-01-10 01:02:05 +0500

    intial code with header and hero banner

---
+++ src/script/script.js
@@ -0,0 +1,22 @@
+// const cursor = document.querySelector(".custom-cursor");
+
+// document.addEventListener("mousemove", (e) => {
+//   cursor.style.left = `${e.pageX}px`;
+//   cursor.style.top = `${e.pageY}px`;
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  hamburger.classList.toggle("active");
+});
+
+document.addEventListener("mousemove", (e) => {
+  const cursor = document.querySelector(".cursor");
+  const trail = document.querySelector(".cursor-trail");
+
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  trail.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
